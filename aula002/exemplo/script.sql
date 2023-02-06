fae066909ab2833c65c96d456f4fc769eb6e530c
isaacgoncod <isaacgoncod@gmail.com>
2023-02-05 23:39:38 -0300

    tarefa

--- aula002/exemplo/script.sql
+++ aula002/exemplo/script.sql
@@ -12,7 +12,15 @@ CREATE TABLE os(
   PRIMARY KEY (id)
 );
 
-ALTER TABLE os DROP funcionario,
+ALTER TABLE os DROP funcionario;
+ALTER TABLE os ADD func_abertura VARCHAR(30) NOT NULL AFTER id;
+ALTER TABLE os ADD func_executor VARCHAR(30) NOT NULL AFTER func_abertura;
+
+INSERT INTO os VALUES
+(null,"Reenye", "Levi", "Torneira da pia da cozinha pingando", "2023-02-01", "11:00:00" , "2023-02-01", "12:00:00"),
+(null,"Paraguassu", "Levi", "Torneira da pia da cozinha pingando", "2023-02-01", "13:00:00" , "2023-02-02", "13:00:00");
+
+
 -- banco de dados ex002/aula001 OS 2.0
 CREATE DATABASE ordens_servico_2;
 USE ordens_servico_2;
@@ -35,3 +43,20 @@ CREATE TABLE os(
   FOREIGN KEY(func_abertura) REFERENCES funcionarios(matricula),
   FOREIGN KEY(func_executor) REFERENCES funcionarios(matricula)
 );
+
+INSERT INTO funcionarios VALUES
+(null, "Levi", "Manutenção"),
+(null, "Denner", "Manutenção"),
+(null, "Reenye", "Professor"),
+(null, "Welliington", "Professor"),
+(null, "Ivo", "Professor"),
+(null, "Paraguassu", "Coordenador"),
+(null, "Tati", "Qualidade");
+
+INSERT INTO os VALUES
+(null, 3, 1, "Torneira da pia da cozinha pingando", "2023-02-01", "11:00:00" , "2023-02-01", "12:00:00"),
+(null, 6, 1, "Torneira da pia da cozinha pingando", "2023-02-01", "13:00:00" , "2023-02-02", "13:00:00");
+
+
+
+
