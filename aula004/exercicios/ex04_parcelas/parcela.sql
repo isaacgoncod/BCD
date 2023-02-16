113d860e741e3938a72d84cebc79276174b0a8e0
isaacgoncod <isaacgoncod@gmail.com>
2023-02-16 11:24:46 -0300

    aula004

--- aula004/exercicios/ex04_parcelas/parcela.sql
+++ aula004/exercicios/ex04_parcelas/parcela.sql
@@ -5,27 +5,27 @@ USE parcelas;
 
 CREATE TABLE cliente(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL,
-  endereco VARCHAR(20) NOT NULL,
-  num_residencia VARCHAR(10) NOT NULL,
-  bairro VARCHAR(20) NOT NULL,
-  cidade VARCHAR(20) NOT NULL,
-  UF VARCHAR(2) NOT NULL
+  nome VARCHAR(255) NOT NULL,
+  rua VARCHAR(255) NOT NULL,
+  num_residencia VARCHAR(255) NOT NULL,
+  complemento VARCHAR(255),
+  bairro VARCHAR(255) NOT NULL,
+  cidade VARCHAR(255) NOT NULL,
+  UF VARCHAR(255) NOT NULL
 );
-create TABLE tel_cliente(
-  id_cliente INT NOT NULL AUTO_INCREMENT,
-  telefone VARCHAR(20) NOT NULL,
-  celular VARCHAR(20) NOT NULL,
+CREATE TABLE tel_cliente(
+  id_cliente INT NOT NULL,
+  telefone VARCHAR(255) NOT NULL,
+  celular VARCHAR(255) NOT NULL,
   FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE TABLE descricao_compra(
-  id_cliente INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE parcela(
+  id_cliente INT NOT NULL,
   num_dupli INT NOT NULL PRIMARY KEY,
   data_compra DATE NOT NULL,
   vencimento DATE NOT NULL,
   pagamento DATE,
   valor FLOAT NOT NULL,
   direfenca FLOAT,
-  v_final FLOAT,
   FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
