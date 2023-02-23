e28dd1107d99d3f9b0192a39ae19d6806eeea918
isaacgoncod <isaacgoncod@gmail.com>
2023-02-23 11:26:45 -0300

    aula006

---
+++ aula004/exercicios/ex01_clientes/data.js
@@ -0,0 +1,11 @@
+function format(data) {
+  let formatData = data.split("/").join("-");
+
+  return formatData;
+}
+
+console.log(format("22/05/2000"));
+console.log(format("22/05/2000"));
+console.log(format("22/05/2000"));
+
+
